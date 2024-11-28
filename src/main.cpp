60358311b7179c5fad0022f7567475e5b9012c81
Bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-11-28 14:58:40 -0500

    Implemented new settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/ProfilePage.hpp>
+#include <Geode/modify/GJGarageLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -19,4 +21,29 @@ class $modify(MenuLayer) {
 
 		return true;
 	} 
+};
+
+class $modify(ProfilePage) {
+	bool init(int accId, bool isSelf) {
+		if (!ProfilePage::init(accId, isSelf)) return false;
+		auto hideOnProfile = Mod::get()->getSettingValue<bool>("hide-profile-page");
+		auto visible = !(isSelf && hideOnProfile);
+		auto menu = this->getChildByType<CCLayer>(0);
+		auto nameMenu = menu->getChildByID("username-menu");
+		auto nameText = nameMenu->getChildByID("username-label");
+		nameText->setVisible(visible);
+
+		return true;
+	}
+};
+
+class $modify(GJGarageLayer) {
+	bool init() {
+		if(!GJGarageLayer::init()) return false;
+		auto usernameLabel = this->getChildByID("username-label");
+		auto hideOnGarage = Mod::get()->getSettingValue<bool>("hide-icon-kit");
+		usernameLabel->setVisible(!hideOnGarage);
+		
+		return true;
+	}
 };
